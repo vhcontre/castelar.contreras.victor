8e2428a4947f03abaa2742a3eda68c274a8aac3b
Victor Contreras <vcontreras@unpaz.edu.ar>
2020-04-23 21:24:54 -0300

    Se agrego la gestión de productos

---
+++ SolutionLPPA/HTML.WebSite/Controllers/ProductoController.cs
@@ -0,0 +1,46 @@
+﻿using HTML.WebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML.WebSite.Controllers
+{
+    public class ProductoController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+
+            var producto = new Producto();
+
+            producto.Codigo = Convert.ToInt32(form["Codigo"]);
+            producto.Nombre = form["Nombre"];
+            producto.Descripcion = form["Descripcion"];
+
+            GestorBiz.Add(producto);
+
+            return RedirectToAction("Index");
+           
+        }
+    }
+}
---
+++ SolutionLPPA/HTML.WebSite/Models/GestorBiz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+
+namespace HTML.WebSite.Models
+{
+    public class GestorBiz
+    {
+        private static List<Producto> productos = new List<Producto>();
+
+
+        public static void InitLista()
+        {
+            productos.Add(new Producto { Codigo = 1234, Nombre = "Teclado", Descripcion = "Descripcion Teclado" });
+            productos.Add(new Producto { Codigo = 1235, Nombre = "Mouse", Descripcion = "Descripcion Mouse" });
+            productos.Add(new Producto { Codigo = 1236, Nombre = "Monitor", Descripcion = "Descripcion Monitor" });
+        }
+
+        public static List<Producto> GetProductos()
+        {
+            if (productos.Count == 0)
+                InitLista();
+
+            return productos;
+        }
+
+        public static void Add(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+    }
+}
