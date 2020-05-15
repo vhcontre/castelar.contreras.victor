d12b4aefb219908b5741ee1d0a05c97ca49fa05a
Victor Contreras <vcontreras@unpaz.edu.ar>
2020-05-14 21:44:04 -0300

    Material de JavaScript

---
+++ SolutionLPPA/HTML.WebSite/Controllers/JavaScriptController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML.WebSite.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Variable()
+        {
+            return View();
+        }
+
+
+        public ActionResult Condicionales()
+        {
+            return View();
+        }
+    }
+}
